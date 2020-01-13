a1c18022e2c7124b7f589c4da47f5bc9f3c8829c
JorisBaudchon <55876397+JorisBaudchon@users.noreply.github.com>
2020-01-13 13:49:48 +0100

    v1 joris

---
+++ siteAPP/views/viewResultatsUtilisateur.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Resultats Utilisateur</title>
+    <link rel="stylesheet" href="views/CSS/CSSviewForum.css">
+</head>
+<body>
+
+</body>
+</html>
